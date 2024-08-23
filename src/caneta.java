e6ab449fdfff8c3ef31185d21ee9e6d9b2ff6c01
J. Henrique Mendes <h06.987654321@gmail.com>
2024-08-23 10:49:58 -0300

    Aula prática GUSTAVO GUANABARA

---
+++ src/caneta.java
@@ -0,0 +1,22 @@
+public class caneta {
+String modelo;
+String cor;
+float ponta;
+int cargaDacaneta;
+boolean tampada;
+
+public static void rabiscar(){
+
+}
+public static void tampar(){
+
+}
+public static void destampar(){
+
+}
+
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ src/teste.java
@@ -0,0 +1,11 @@
+public class teste {
+    public static void main (String[]args){
+        caneta can = new caneta();
+        can.cor = "Azul";
+        can.ponta = 0.5f;
+        can.cargaDacaneta = 20;
+        can.modelo = "Bic";
+        can.tampada =  false;
+
+    }
+}
